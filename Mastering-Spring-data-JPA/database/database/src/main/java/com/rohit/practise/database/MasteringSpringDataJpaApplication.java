e340e7399d72bf960ee33c36066a1b069483cab0
Shalgar <RShalgar@luxoft.com>
2022-02-07 13:35:20 -0600

    First commit on the new project

---
+++ Mastering-Spring-data-JPA/database/database/src/main/java/com/rohit/practise/database/MasteringSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.rohit.practise.database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasteringSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasteringSpringDataJpaApplication.class, args);
+	}
+
+}
